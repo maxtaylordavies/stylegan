d58c8213bc9df9a08cf8ffc918d6f7471849a123
Max Taylor-Davies <maxtaylordavies@gmail.com>
2020-06-09 16:11:14 +0100

    adds shell script for looped training with autorestart

---
+++ test.py
@@ -0,0 +1,5 @@
+def main():
+    print("pythom")
+
+if __name__ == "__main__":
+    main()
---
+++ train.sh
@@ -0,0 +1,1 @@
+while true; do nice python train.py ; date; echo "alert: StyleGAN crashed"; sleep 10s; done
