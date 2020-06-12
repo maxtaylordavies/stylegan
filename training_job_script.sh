dcf004a2123a47d4e4c00187761f71f895d34b09
Max Taylor-Davies <maxtaylordavies@gmail.com>
2020-06-12 11:53:16 +0100

    switches from 8 gpus to 2

--- training_job_script.sh
+++ training_job_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #PBS -l walltime=24:00:00
-#PBS -lselect=1:ncpus=32:mem=192gb:ngpus=8:gpu_type=RTX6000
+#PBS -lselect=1:ncpus=8:mem=48gb:ngpus=2:gpu_type=RTX6000
 
 module load anaconda3/personal
 source activate biggan
